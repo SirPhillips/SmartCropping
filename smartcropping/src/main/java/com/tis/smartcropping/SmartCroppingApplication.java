7883fafe57286b2ac3dd3c2b2d3160d378645eb3
SirPhillips <felipcas@amdocs.com>
2019-06-10 15:33:22 -0400

    First commit
    First commit

---
+++ smartcropping/src/main/java/com/tis/smartcropping/SmartCroppingApplication.java
@@ -0,0 +1,13 @@
+package com.tis.smartcropping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCroppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCroppingApplication.class, args);
+	}
+
+}
---
+++ smartcropping/src/test/java/com/tis/smartcropping/SmartCroppingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tis.smartcropping;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartCroppingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
